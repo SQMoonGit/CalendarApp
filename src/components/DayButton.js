3ab5b6c4f598843a0f82e76c67c25fd12fafb96e
SQMoonGit <hykmoon@gmail.com>
2021-02-25 21:54:44 -0600

    More calendar styling

--- src/components/DayButton.js
+++ src/components/DayButton.js
@@ -3,9 +3,9 @@ import styled from 'styled-components/native';
 
 const Wrapper = styled.TouchableOpacity`
   flex-direction: row;
+  flex-basis: 14.2%;
+  min-height: 100px;
   border: 0.5px solid black;
-  width: 14%;
-  height: 100%;
 `;
 
 const Title = styled.Text`
@@ -16,10 +16,10 @@ const Title = styled.Text`
 const DayButton = (props) => {
   return (
     <Wrapper
-      activeOpacity={props.opacity}
+      activeOpacity={props.opacity ? props.opacity : 0.6}
       onPress={props.onPress}
       active={props.active}
-      style={{backgroundColor: props.color}}>
+      style={props.style ? props.style : {backgroundColor: 'white'}}>
       <Title style={{color: 'black'}}>{props.title}</Title>
     </Wrapper>
   );
--- src/components/MonthFormat.js
+++ src/components/MonthFormat.js
@@ -1,4 +1,5 @@
 import DayButton from './DayButton';
+import {StyleSheet} from 'react-native';
 import React from 'react';
 import styled from 'styled-components/native';
 
@@ -9,6 +10,22 @@ const Wrapper = styled.View`
   flex-wrap: wrap;
 `;
 
+const Row = styled.View`
+  flex-direction: row;
+  flex: 1;
+  flex-wrap: wrap;
+`;
+
+const styles = StyleSheet.create({
+  borderSide: {
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+  },
+  borderTop: {
+    borderTopWidth: 0,
+  },
+});
+
 const MonthFormat = (props) => {
   const daysInMonth = new Date(
     props.date.getFullYear(),
@@ -17,16 +34,28 @@ const MonthFormat = (props) => {
   ).getDate();
   let days = [];
   for (let x = 1; x <= daysInMonth; x++) {
-    days.push(
-      <DayButton
-        opacity={0.6}
-        color="white"
-        key={x}
-        title={x.toString()}></DayButton>,
-    );
+    if (x % 7 === 1) {
+      days.push(
+        <DayButton
+          style={styles.borderTop}
+          key={x}
+          title={x.toString()}></DayButton>,
+      );
+    } else {
+      days.push(
+        <DayButton
+          style={styles.borderSide}
+          key={x}
+          title={x.toString()}></DayButton>,
+      );
+    }
   }
 
-  return <Wrapper>{days}</Wrapper>;
+  return (
+    <Wrapper>
+      <Row>{days}</Row>
+    </Wrapper>
+  );
 };
 
 export default MonthFormat;
